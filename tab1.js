9340340277b949854d926bda0f7b90ab0d47127b
whsjtu <whsjtuis@gmail.com>
2013-04-19 15:57:51 +0800

    4.19

--- tab1.js
+++ tab1.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
-	$('#tabs').tabs({
-		fx : { opacity: 'toggle'},
-	}).tabs("add", "#tab4", "Tab 4");
+	$('#tabs').tabs();
 
-	//$("#tab4").text("Content of the fourth tab");
-	$("<i>Contents of the fourth tab</i>").appendTo('#tab4');
+	$('#accordion').accordion({icons: {'header': 'ui-icon-plus', 'activeHeader': 'ui-icon-minus'}});
+	//$('#accordion').accordion("destroy");
+    //alert($('#accordion').accordion('option'));
+
+   /* $('#dialog').dialog({autoOpen: false});
+   
+    $('#open').click(function(event){
+    	if($('#dialog').dialog('isOpen'))
+    		alert('Already open !');
+    	else $('#dialog').dialog('open');
+    });
+   
+    $('#close').click(function(event){
+    	if(!$('#dialog').dialog('isOpen'))
+    		alert('Already closed !');
+    	else $('#dialog').dialog('close');
+    });
+   */
+    $('#dialog').dialog({
+    	show: "bounce",
+    	hide: "pulsate"
+    }).dialog({
+    	beforeClose: function(event){
+    		if(!confirm('Close dialog?'))
+    			return false;
+    	}	
+    }); 
 });
