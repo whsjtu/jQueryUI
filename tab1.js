3b5e6cea50b46cb419f684062470d6560bf2e4a0
whsjtu <whsjtuis@gmail.com>
2013-04-22 17:30:07 +0800

    4.22

--- tab1.js
+++ tab1.js
@@ -41,4 +41,78 @@ $(document).ready(function(){
     }).prev().find('.ui-dialog-titlebar-close').hide(); */
 
     $('#button1, #button2').button();
+    $('input').button();
+    $('.buttonset').buttonset();
+    $('#checkbox input').button();
+
+    $('#volume').button({
+        icons: {
+            secondary: 'ui-icon-volume-off'
+        }, 
+        label: 'volume off'
+    }).click(function(event){
+        var volume = $(this).button('option', 'icons').secondary == 'ui-icon-volume-off' ? false : true;
+        if (volume) {
+            $(this).button('option', 'label', 'volume off');
+            $(this).button('option', 'icons', {secondary: 'ui-icon-volume-off'});
+        } else {
+            $(this).button('option', 'label', 'volume on');
+            $(this).button('option', 'icons', {secondary: 'ui-icon-volume-on'});
+        }
+    });
+    $('#progressbar').progressbar({
+        value: false
+    });
+   /* $('#progressbar').progressbar({
+        change: function(event){
+        var value = $('#progressbar').progressbar('value');
+        $('#percent').html(value + ' %');
+        }
+    });
+
+    var value = 0;
+
+    var timer = setInterval(function(){
+        $('#progressbar').progressbar('value', value);
+        value ++;
+        if(value > 100) clearInterval(timer);
+    }, 10);
+
+*/
+/*
+    $('#slider').slider({
+        animate: true,
+        range: true,
+        values: [0, 0],
+        start: function(event){
+            var values = $('#slider').slider('values');
+            $('#valuestart').html(values[0] + ', ' + values[1]);
+        },
+        stop: function(event){
+            var values = $('#slider').slider('values');
+            $('#valuestop').html(values[0] + ', ' + values[1]);
+        },
+        change: function(event){
+            var values = $('#slider').slider('values');
+            $('#valuechange').html(values[0] + ', ' + values[1]);
+        },
+        slide: function(event){
+            var values = $('#slider').slider('values');
+            $('#valueslide').html(values[0] + ', ' + values[1]);
+        }
+    });
+*/
+    $('#slider').slider({
+        animate: true,
+        slide: function(){
+            var value = $('#slider').slider('value');
+            var opacity = value/100;
+            $('img').css({opacity: opacity});          
+        }
+    }).slider('value', 100);
+
+
+    $('#date').datepicker({
+        showAnim: 'bounce'
+    });
 });
